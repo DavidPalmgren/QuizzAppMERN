4ccd564fcc3f63b41fada7a4de6230365f5e19df
Dapa22 <dapa22@student.bth.se>
2023-09-05 05:42:41 +0200

    asdfgsdg

--- frontend/my-react-app/src/components/HomePage.js
+++ frontend/my-react-app/src/components/HomePage.js
@@ -5,40 +5,29 @@ function HomePage() {
   const [cards, setCards] = useState([]);
 
   useEffect(() => {
-    axios.get('https://studyapp-dapa-98dcdc34bdde.herokuapp.com/api/cards')
-      .then((response) => {
+    async function fetchData() {
+      try {
+        const response = await axios.get('https://studyapp-dapa-98dcdc34bdde.herokuapp.com/api/cards');
         console.log('API Response:', response.data); // Log the response data
         setCards(response.data);
-      })
-      .catch((error) => {
+      } catch (error) {
         console.error('Error fetching data:', error);
-      });
+      }
+    }
+
+    fetchData();
   }, []);
 
   return (
     <div>
       <h1>Welcome to My Landing Page</h1>
       <ul>
-        {cards.length === 0 ? (
-          <li>No cards available</li>
-        ) : (
-          createListItems(cards)
-        )}
+        {cards.length === 0 ? <li>No cards available</li> : cards.map((card) => (
+          <li key={card._id}>{card.name}</li>
+        ))}
       </ul>
     </div>
   );
-}
-
-// Function to create list items
-function createListItems(cards) {
-  const listItems = [];
-  for (let i = 0; i < cards.length; i++) {
-    const card = cards[i];
-    listItems.push(
-      <li key={card._id}>{card.name}</li>
-    );
-  }
-  return listItems;
 }
 
 export default HomePage;
