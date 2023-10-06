e0f071a7e020bdf6a174a6f87c6967991f20cffd
Dapa22 <dapa22@student.bth.se>
2023-10-06 18:39:55 +0200

    url signup fix

--- frontend/my-react-app/src/view/SignUp.js
+++ frontend/my-react-app/src/view/SignUp.js
@@ -26,7 +26,7 @@ export default function SignUp() {
     };
 
     try {
-      const response = await fetch('lhttps://studyapp-dapa-98dcdc34bdde.herokuapp.com/register', {
+      const response = await fetch('https://studyapp-dapa-98dcdc34bdde.herokuapp.com/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
